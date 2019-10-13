ee773300dc9c804fbf526083f30583d39dc807c2
juliodelfino <julius.delfino@yahoo.com>
2019-10-13 10:44:20 +0800

    Changed API server

--- sgbus.php
+++ sgbus.php
@@ -1,5 +1,5 @@
  <?php 
  	parse_str($_SERVER['QUERY_STRING']);
- 	$sgbus_info = file_get_contents('https://arrivelah.herokuapp.com/?id=' . $id); 
+ 	$sgbus_info = file_get_contents('https://sagabus.herokuapp.com/bus?id=' . $id);
   print_r($sgbus_info);
  ?> 
